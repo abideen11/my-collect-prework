7ed2b3d358de264b18e387313e7f3fb14ce69577
Abideen Kareem <abideenkareem17@gmail.com>
2019-06-10 17:54:33 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(collection)
  i = 0 
  new_collection = []
  while i < collection.count
- yield(collection[i])
- new_collection.push()
+ #yield(collection[i])
+ new_collection.push(yield(collection[i]))
  i += 1 
  end 
  collection 
